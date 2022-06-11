bf0433e1bc0cf3948c444afc6b84b99a00c684a3
evelynfoy <evelyn.foy@hotmail.com>
2022-06-11 20:03:20 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L9ZNvHmi0FrXN9dAl2kCbfCO5q2ycpFV3fMfZEJb27Z7h2zh6nhnArwb7hKSd2SWnAEncS5wFZyyaMENQ7toxCZ00qHrs6Hk3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
