d4ec0faec85122fd5c197cf9dd743cb54b0c5a20
evelynfoy <evelyn.foy@hotmail.com>
2022-06-12 10:57:50 +0000

    Added basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -169,7 +169,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
