ccd2b905a0594f4a4fbad795435679e57d20a1b3
evelynfoy <evelyn.foy@hotmail.com>
2022-06-13 07:57:52 +0000

    Added loading overlay

--- checkout/views.py
+++ checkout/views.py
@@ -103,6 +103,10 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
