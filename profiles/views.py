f6b69a779fe6b4208767f70b82daf50a8922f796
evelynfoy <evelyn.foy@hotmail.com>
2022-06-17 19:31:13 +0000

    Added profile app

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def profile(request):
+    """ Display users profile """
+    template = 'profiles/profile.html'
+    context = {
+
+    }
+    return render(request, template, context)
