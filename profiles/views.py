c30a83d942eb38410d9b5ba2e58a3136a194a133
evelynfoy <evelyn.foy@hotmail.com>
2022-06-18 20:09:47 +0000

    added order history and updated chedkout views.

--- profiles/views.py
+++ profiles/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
-from checkout.models import Order
 from .forms import UserProfileForm
+
+from checkout.models import Order
+
 
 def profile(request):
     """ Display users profile """
@@ -22,6 +24,23 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
     return render(request, template, context)
